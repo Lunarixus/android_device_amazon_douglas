752cd8b79b34a9239ee8c1dc3aa44ee110d4abba
Lunarixus <Nathanb644@hotmail.co.uk>
2020-05-13 13:18:50 +0000

    douglas: Build memtrack HAL
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- device.mk
+++ device.mk
@@ -91,7 +91,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl \
     libion \
     libgralloc_extra \
     libGLES_android \
@@ -99,6 +98,8 @@ PRODUCT_PACKAGES += \
 
 # Memtrack
 PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     memtrack.mt8163
 
 # Lights
