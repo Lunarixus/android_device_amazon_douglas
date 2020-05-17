1e316ae2137bb0668da62b08e4d71f748fc3597e
Lunarixus <Nathanb644@hotmail.co.uk>
2020-05-17 22:27:24 +0000

    douglas: Add media seccomp-policy
    From https://github.com/mt8127/android_device_alcatel_ttab/tree/lineage-15.1/seccomp
    
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- device.mk
+++ device.mk
@@ -55,7 +55,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/audio_device.xml:system/etc/audio_device.xml \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/vendor/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
 
 # Audio
 PRODUCT_PACKAGES += \
