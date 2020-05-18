mt8127/android_device_alcatel_ttab

releasetools: force flash boot.img
for unknown reasons `package_extract_file("boot.img", "/dev/bootimg");` is doing nothing at 99% of the time

Change-Id: I74782867c3eb2a7039dbe16b2217eee9518580a6

BoardConfig.mk
@@ -118,3 +118,5 @@ TARGET_LD_SHIM_LIBS := \
 # SELinux
 BOARD_SECCOMP_POLICY := $(DEVICE_DIR)/seccomp
 #BOARD_SEPOLICY_DIRS += $(DEVICE_DIR)/sepolicy
+
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_DIR)/releasetools

Lunarixus/android_device_amazon_douglas

BoardConfig.mk
@@ -101,6 +101,9 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # Use dlmalloc instead of jemalloc for mallocs
 MALLOC_SVELTE := true
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)/releasetools
+
 # Seccomp
 BOARD_SECCOMP_POLICY := $(LOCAL_PATH)/seccomp
 